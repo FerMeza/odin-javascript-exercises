88fbb227efe75575be6f72b0e0d1c3eb9b87d59e
FerMeza <30847755+FerMeza@users.noreply.github.com>
2024-10-14 00:12:53 -0600

    feat: implementation exercise 4 & 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    if (firstNumber < 0
+        || secondNumber < 0
+        || !Number.isInteger(firstNumber)
+        || !Number.isInteger(secondNumber))
+    return "ERROR";
+    let startNumber = 0, endNumber = 0;
+    if (firstNumber > secondNumber) {
+        startNumber = secondNumber;
+        endNumber = firstNumber;
+    } else {
+        startNumber = firstNumber;
+        endNumber = secondNumber;
+    }
+    let sum = 0;
+    for (let currentNumber = startNumber; 
+        currentNumber <= endNumber; 
+        currentNumber++) {
+        sum += currentNumber;
+    }
+    return sum;
 };
 
 // Do not edit below this line
