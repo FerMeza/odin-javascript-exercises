8683b0770f5ac8bf11d4141937f911c44f2c190f
FerMeza <30847755+FerMeza@users.noreply.github.com>
2024-10-14 17:48:50 -0600

    feat: implement exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farenheitDegrees) {
+  const decimalPlaces = 1;
+  let celciusDegrees = (farenheitDegrees - 32) * (5 / 9);
+  return Number.isInteger(celciusDegrees) ?
+  celciusDegrees :
+  +celciusDegrees.toFixed(decimalPlaces);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celciusDegrees) {
+  const decimalPlaces = 1;
+  let farenheitDegrees = (9 / 5) * celciusDegrees + 32;
+  return Number.isInteger(farenheitDegrees) ?
+  farenheitDegrees :
+  +farenheitDegrees.toFixed(decimalPlaces);
 };
 
 // Do not edit below this line
